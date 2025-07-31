00cfd86ce154233261c8394e9e72cd69649bf374
Erik Kauê <kaue_erik122@hotmail.com>
2025-07-30 21:45:50 -0300

    Fundamentos - Organizando o projeto

--- App.tsx
+++ App.tsx
@@ -1,25 +1,5 @@
-import { StyleSheet, Text, View } from "react-native";
+import Home from "./src/screens/Home/Index";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text style={styles.eventName}>Nome do evento</Text>
-      <Text style={styles.eventDate}>Sexta, 4 de Novembro de 2025</Text>
-    </View>
-  );
+  return <Home />;
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "black",
-    padding: 24,
-  },
-  eventName: {
-    color: "white",
-    fontSize: 24,
-    fontWeight: "bold",
-    marginTop: 48,
-  },
-  eventDate: { color: "gray", fontSize: 16 },
-});
---
+++ src/screens/Home/styles.ts
@@ -0,0 +1,16 @@
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "black",
+    padding: 24,
+  },
+  eventName: {
+    color: "white",
+    fontSize: 24,
+    fontWeight: "bold",
+    marginTop: 48,
+  },
+  eventDate: { color: "gray", fontSize: 16 },
+});
