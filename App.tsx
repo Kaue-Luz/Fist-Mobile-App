19188088b75b5c155bca7bde9a6c5fcdb4dbd91a
Erik Kauê <kaue_erik122@hotmail.com>
2025-07-30 21:35:13 -0300

    Fundamentos - Utilizando StyleSheet

--- App.tsx
+++ App.tsx
@@ -1,11 +1,25 @@
-import { Text, View } from "react-native";
+import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
   return (
-    <View>
-      <Text>Hello World!!</Text>
+    <View style={styles.container}>
+      <Text style={styles.eventName}>Nome do evento</Text>
+      <Text style={styles.eventDate}>Sexta, 4 de Novembro de 2025</Text>
     </View>
   );
 }
 
-
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "black",
+    padding: 24,
+  },
+  eventName: {
+    color: "white",
+    fontSize: 24,
+    fontWeight: "bold",
+    marginTop: 48,
+  },
+  eventDate: { color: "gray", fontSize: 16 },
+});
