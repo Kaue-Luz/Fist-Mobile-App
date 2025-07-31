84501bed7a629bfc35e24d25f935abe22f0d31f8
Erik Kauê <kaue_erik122@hotmail.com>
2025-07-31 19:43:18 -0300

    Fundamentos - Input de dados

--- App.tsx
+++ App.tsx
@@ -1,4 +1,4 @@
-import Home from "./src/screens/Home/Index";
+import Home from "./src/screens/Home";
 
 export default function App() {
   return <Home />;
--- src/screens/Home/styles.ts
+++ src/screens/Home/styles.ts
@@ -7,10 +7,18 @@ export const styles = StyleSheet.create({
     padding: 24,
   },
   eventName: {
-    color: "white",
+    color: "#ffff",
     fontSize: 24,
     fontWeight: "bold",
     marginTop: 48,
   },
   eventDate: { color: "gray", fontSize: 16 },
+  input: {
+    height: 56,
+    backgroundColor: "#1f1e25",
+    borderRadius: 5,
+    color: "#ffff",
+    padding: 16,
+    fontSize: 16,
+  },
 });
